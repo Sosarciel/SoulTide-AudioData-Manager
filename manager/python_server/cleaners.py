18755cc10be2374f3a0d9b2b644502864c4da7bf
zwa73 <ssfen2419@126.com>
2025-05-15 17:15:37 +0800

    fx clear

--- manager/python_server/cleaners.py
+++ manager/python_server/cleaners.py
@@ -8,7 +8,18 @@ _japanese_characters = re.compile(r'[A-Za-z\d\u3005\u3040-\u30ff\u4e00-\u9fff\uf
 # Regular expression matching non-Japanese characters or punctuation marks:
 _japanese_marks = re.compile(r'[^A-Za-z\d\u3005\u3040-\u30ff\u4e00-\u9fff\uff11-\uff19\uff21-\uff3a\uff41-\uff5a\uff66-\uff9d]')
 
-def japanese_cleaners(text):
+# List of (symbol, Japanese) pairs for marks:
+_symbols_to_japanese = [(re.compile('%s' % x[0]), x[1]) for x in [
+    ('％', 'パーセント')
+]]
+
+def symbols_to_japanese(text):
+    for regex, replacement in _symbols_to_japanese:
+        text = re.sub(regex, replacement, text)
+    return text
+
+def japanese_to_romaji_with_accent(text):
+  text = symbols_to_japanese(text)
   sentences = re.split(_japanese_marks, text)
   marks = re.findall(_japanese_marks, text)
   text = ''
@@ -23,7 +34,7 @@ def japanese_cleaners(text):
           text += phoneme.replace('ch','ʧ').replace('sh','ʃ').replace('cl','Q')
         else:
           continue
-        n_moras = int(re.search(r'/F:(\d+)_', label).group(1))
+        # n_moras = int(re.search(r'/F:(\d+)_', label).group(1))
         a1 = int(re.search(r"/A:(\-?[0-9]+)\+", label).group(1))
         a2 = int(re.search(r"\+(\d+)\+", label).group(1))
         a3 = int(re.search(r"\+(\d+)/", label).group(1))
@@ -35,13 +46,22 @@ def japanese_cleaners(text):
         if a3 == 1 and a2_next == 1:
           text += ' '
         # Falling
-        elif a1 == 0 and a2_next == a2 + 1 and a2 != n_moras:
+        #elif a1 == 0 and a2_next == a2 + 1 and a2 != n_moras:
+        #  text += '↓'
+        # Falling
+        elif a1 == 0 and a2_next == a2 + 1:
           text += '↓'
         # Rising
         elif a2 == 1 and a2_next == 2:
           text += '↑'
-    if i<len(marks):
+    if i < len(marks):
       text += unidecode(marks[i]).replace(' ','')
-  if re.match('[A-Za-z]',text[-1]):
-    text += '.'
   return text
+
+def japanese_cleaners(text):
+    text = japanese_to_romaji_with_accent(text)
+    text = re.sub(r'([A-Za-z])$', r'\1.', text)
+    return text
+
+def japanese_cleaners2(text):
+    return japanese_cleaners(text).replace('ts', 'ʦ').replace('...', '…')
