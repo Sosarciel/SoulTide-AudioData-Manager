7b902d31b961ba17f0bbd671d2f8e6fce4dc9c92
zwa73 <ssfen2419@126.com>
2025-07-23 21:58:41 +0800

    fx

--- manager/start_python_server.ps1
+++ manager/start_python_server.ps1
@@ -1,3 +1,3 @@
 ﻿conda activate ./minconda
 Start-Sleep -Seconds 1
-uvicorn python_server.server:app --host 127.0.0.1 --port 4242 --workers 4
+python -m uvicorn python_server.server:app --host 127.0.0.1 --port 4242 --workers 4
